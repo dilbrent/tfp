644ba9bc8a2a19c58e5a3a03559bda1dff1aacf0
Dilbrent <dilbrent@gmail.com>
2018-07-29 15:04:45 -0700

    moving tests out to script

--- train.sh
+++ train.sh
@@ -16,11 +16,4 @@ python3 -m scripts.retrain \
   --architecture="${ARCHITECTURE}" \
   --image_dir=tf_files/training_pics
 
-python3 -m scripts.label_image     --graph=tf_files/retrained_graph.pb      \
-  --image=tf_files/training_pics/LegoBB8/images_079.jpg
-
-python3 -m scripts.label_image     --graph=tf_files/retrained_graph.pb      \
-  --image=tf_files/tests/test_blurrycoke.jpg 
-
-python3 -m scripts.label_image     --graph=tf_files/retrained_graph.pb      \
-  --image=tf_files/tests/test_goodtardis.jpg 
+./doTests.sh
