d726842bde43f0b30ce1cf12ad9f494fa2723f04
Dilbrent <dilbrent@gmail.com>
2018-07-31 04:43:59 -0700

    Added products and improved scripts

--- video/extract.sh
+++ video/extract.sh
@@ -1,4 +1,5 @@
 mkdir -p frames
+rm -r frames/*
 if [ "$1" != "" ]; then
   ffmpeg -i $1 -vf scale="640:-1" frames/out%05d.jpg
 fi
--- video/pull.sh
+++ video/pull.sh
@@ -1,5 +1,6 @@
 adb shell ls /storage/103A-B388/DCIM/Camera/*.mp4
 if [ "$1" != "" ]; then
-  adb pull $1
+  adb pull /storage/103A-B388/DCIM/Camera/$1
+  ./extract.sh $1
 fi
 
